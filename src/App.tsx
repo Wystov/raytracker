59a32b7f2b9a826dbe654b4c4bf1bf278d66af29
Wystov <wystov@gmail.com>
2024-12-26 00:07:26 +0100

    feat: add dark theme and switch btn

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,26 @@ import { Lamp } from '@/components/lamp';
 import { Login } from '@/components/login';
 import { user } from '@/store/user';
 
+import { ThemeProvider } from './components/theme-provider';
+import { ThemeToggle } from './components/theme-toggle';
+
 export const App = observer(function App() {
   return (
-    <main
-      className="flex flex-1 flex-col items-center p-4 max-w-fit mx-auto"
-      data-vaul-drawer-wrapper
-    >
-      {user.initialized ? (
-        <>
-          <Login />
-          {user.profile && <Lamp />}
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </main>
+    <ThemeProvider defaultTheme="system" storageKey="raytracker-theme">
+      <main
+        className="flex flex-1 flex-col items-center p-4 max-w-fit mx-auto"
+        data-vaul-drawer-wrapper
+      >
+        <ThemeToggle />
+        {user.initialized ? (
+          <>
+            <Login />
+            {user.profile && <Lamp />}
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </main>
+    </ThemeProvider>
   );
 });
