0fc297f1cfb190ee51caf2ce342705bd69a4d85a
Wystov <wystov@gmail.com>
2025-01-02 05:37:51 +0100

    feat: tabs for table and calendar session views

--- src/components/sessions.tsx
+++ src/components/sessions.tsx
@@ -6,6 +6,7 @@ import { sessions } from '@/store/sessions';
 import { SessionDrawer } from './session-drawer';
 import { SessionsTable } from './sessions-table';
 import { Drawer } from './ui/drawer';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 export const Sessions = observer(function SessionsList() {
   return (
@@ -17,7 +18,22 @@ export const Sessions = observer(function SessionsList() {
         </Drawer>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
-        {sessions.list.length ? <SessionsTable /> : <p>No records</p>}
+        {sessions.list.length ? (
+          <Tabs defaultValue="table">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="table">Table</TabsTrigger>
+              <TabsTrigger value="Calendar">Calendar</TabsTrigger>
+            </TabsList>
+            <TabsContent value="table">
+              <SessionsTable />
+            </TabsContent>
+            <TabsContent value="Calendar">
+              <p>Calendar</p>
+            </TabsContent>
+          </Tabs>
+        ) : (
+          <p>No records</p>
+        )}
       </CardContent>
     </Card>
   );
