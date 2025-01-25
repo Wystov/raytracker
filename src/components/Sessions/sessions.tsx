f87d8ef2dc56ccf883573b12d55f423208c58eba
Wystov <wystov@gmail.com>
2025-01-25 09:57:42 +0100

    fix: imports

--- src/components/Sessions/sessions.tsx
+++ src/components/Sessions/sessions.tsx
@@ -1,8 +1,8 @@
 import { observer } from 'mobx-react-lite';
 
-import { SessionDrawer } from '@/components/session-drawer';
-import { SessionsCalendar } from '@/components/sessions-calendar';
-import { SessionsTable } from '@/components/sessions-table';
+import { SessionDrawer } from '@/components/Sessions/SessionDrawer/session-drawer';
+import { SessionsCalendar } from '@/components/Sessions/sessions-calendar';
+import { SessionsTable } from '@/components/Sessions/SessionsTable/sessions-table';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Drawer } from '@/components/ui/drawer';
 import { Loader } from '@/components/ui/loader';
